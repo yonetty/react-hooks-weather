ae31e32b5d130a8293080277a425dd3764767dfb
yonetty <tyonekubo@outlook.jp>
2020-06-15 00:24:57 +0900

    day 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,17 @@
-import React from 'react';
-import { Forcast } from './components/Forcast';
+import React, { useState } from 'react';
 import { CitySelector } from './components/CitySelector';
+import { Forcasts } from './components/Forcasts';
 
 function App() {
-  const forcasts = [
-    {
-      dateLabel: "今日",
-      telop: "雨のち曇",
-      date: "2020-06-14",
-      maxTemperature: "27",
-      imageUrl: "http://weather.livedoor.com/img/icon/20.gif"
-    },
-    {
-      dateLabel: "明日",
-      telop: "雨のち曇",
-      date: "2020-06-15",
-      minTemperature: "24",
-      maxTemperature: "31",
-      imageUrl: "http://weather.livedoor.com/img/icon/20.gif"
-    },
-  ]
+  const [cityCode, setCityCode] = useState("");
+  const handleCityChange = (cityCode: string) => {
+    setCityCode(cityCode);
+  }
+
   return (
     <div>
-      <CitySelector />
-      {forcasts.map((f) => {
-        return <Forcast key={f.date} {...f} />
-      })}
+      <CitySelector onCityChange={handleCityChange} />
+      <Forcasts cityCode={cityCode} />
     </div>
   );
 }
